f4f16f98f7db2349cc899a3b704708fe9a690b86
DeusMaximus <DeusMaximus@users.noreply.github.com>
2022-01-27 12:19:22 +1000

    Updated to create generic uninstaller script via winget

--- wingetSysCmd.ps1
+++ wingetSysCmd.ps1
@@ -1,3 +1,27 @@
+$scriptContents = @'
+$WingetCmd = Get-Command winget.exe -ErrorAction SilentlyContinue
+
+if ($WingetCmd){
+    $wingetsys = $WingetCmd.Source
+}
+elseif (Test-Path  "$env:ProgramW6432\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\AppInstallerCLI.exe"){
+    $wingetsys = Resolve-Path  "$env:ProgramW6432\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\AppInstallerCLI.exe" | Select-Object -ExpandProperty Path
+}
+elseif (Test-Path  "$env:ProgramW6432\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"){
+    $wingetsys = Resolve-Path  "$env:ProgramW6432\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe" | Select-Object -ExpandProperty Path
+}
+else{
+    Write-Host "WinGet not Installed."
+    return
+}
+
+& $wingetsys $args
+'@
+
+$Uninstaller = "$Env:PROGRAMDATA\WinGetSys\wingetSysCmd.ps1"
+
+New-Item $Uninstaller -ItemType File -Value $scriptContents -Force
+
 $WingetCmd = Get-Command winget.exe -ErrorAction SilentlyContinue
 
 if ($WingetCmd){
